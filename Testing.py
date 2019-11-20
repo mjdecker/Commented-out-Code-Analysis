8cd5b8939bbd8483a402f199e55d79f4a007bf7c
Michael John Decker <mdecker6@kent.edu>
2019-11-20 15:17:10 -0500

    Put results in dict and print

--- Testing.py
+++ Testing.py
@@ -10,16 +10,20 @@ print(letterFreqData)
 
 y = letterFreqData["target"]
 X = letterFreqData["data"]
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
-dt = dt.fit(X, y)
+dt = dt.fit(X_train, y_train)
 
 dot_data = export_graphviz(dt, feature_names=letterFreqData["feature_names"], out_file=None, filled=True, class_names=letterFreqData["target_names"])
 graph = graphviz.Source(dot_data) 
 graph.render("comments_decision_tree") 
 
-#y_pred = dt.predict(X_test)
+y_pred = dt.predict(X_test)
 
-#print("accuracy:", metrics.accuracy_score(y_test, y_pred))
-#print("F1:", metrics.f1_score(y_test, y_pred))
+results = { "accuracy":  metrics.accuracy_score(y_test, y_pred),
+		    "precision": metrics.precision_score(y_test, y_pred),
+ 		    "recall":    metrics.recall_score(y_test, y_pred),
+ 		    "F1":        metrics.f1_score(y_test, y_pred)
+		  }
 
+[print(key + ":", results[key]) for key in results]
